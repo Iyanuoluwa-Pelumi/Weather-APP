966b7fd32baff9d006465cf0dbba5e5aab652ea8
Iyanuoluwa-Pelumi <ojoiyanuoluwa1@gmail.com>
2025-08-15 15:37:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const fetchBtn = document.querySelector('.fetch-btn');
+const cityInput = document.querySelector('.city-name');
+
+fetchBtn.addEventListener('click', async() => {
+    const cityName = document.querySelector('.city-name').value.trim();
+    const apiKey = '86c4d81d5c9a7108241f98962410c458'
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    if (!cityName) {
+        alert('Enter a City Name');
+        return;
+    }
+
+    document.querySelector('.result').textContent = 'Loading...'
+
+    try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error(`Error: ${res.status}`);
+        const data = await res.json();
+
+        console.log(data)
+
+        document.querySelector('.result').innerHTML = `
+            <h2 class="cityName">${data.name}, ${data.sys.country}</h2>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Weather Condition: ${data.weather[0].description}.</p>
+        `;
+    } catch (err) {
+        document.querySelector('.result').innerHTML = err.message;
+    }
+
+    cityInput.value = '';
+})
